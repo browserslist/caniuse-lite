61ec29cad3c0fda66eccc5d9c78c025cef9ad51c
Andrey Sitnik <andrey@sitnik.ru>
2021-05-10 01:19:21 -0400

    Simplify script

--- log.js
+++ log.js
@@ -1,9 +1,8 @@
 const { readFile } = require('fs').promises
-const { red } = require('colorette')
 
 readFile('./publish.log').then(log => {
   if (log.toString().includes('npm ERR!')) {
-    process.stderr.write(red(log.toString()))
+    process.stderr.write(log.toString())
     process.exit(1)
   }
 })
