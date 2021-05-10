fde289588b2ccb129ba3d1552134be2c78fee8b7
Andrey Sitnik <andrey@sitnik.ru>
2021-05-10 02:22:05 -0400

    Fix exports

--- src/unpacker/index.js
+++ src/unpacker/index.js
@@ -1,4 +1,4 @@
-module.exports.agents = require('./agents')
+module.exports.agents = require('./agents').agents
 module.exports.feature = require('./feature')
-module.exports.features = require('./features')
+module.exports.features = require('./features').features
 module.exports.region = require('./region')
