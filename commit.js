20541ac49cdf9947272937798839fa0d8c326978
Dominik George <nik@naturalnet.de>
2021-10-04 15:00:12 +0200

    Also tag the current db version after commit
    Closes #84.

--- commit.js
+++ commit.js
@@ -4,3 +4,4 @@ const pkg = require('./package.json')
 
 execSync('git add data/ package.json yarn.lock')
 execSync(`git commit -m "Update caniuse-db ${pkg.version}"`)
+execSync(`git tag -m "Release caniuse-db ${pkg.version}" ${pkg.version}`)
