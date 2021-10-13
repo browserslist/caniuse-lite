429688828957f6a49231b639bce06e4edb6dbf96
Luke Warlow <projects@warlow.dev>
2021-10-13 22:05:38 +0100

    Use MDN data to generate :autofill support data

--- check.js
+++ check.js
@@ -1,5 +1,6 @@
 const { writeFile } = require('fs')
 const { get } = require('https')
+const bcd = require('@mdn/browser-compat-data')
 
 const pkg = require('./package.json')
 
@@ -27,3 +28,50 @@ get('https://registry.npmjs.org/caniuse-db', res => {
     }
   })
 })
+
+const autofillSupportData = bcd.css.selectors.autofill.__compat.support
+
+function bcdBrowserToCanIUseBrowser(bcdBrowser) {
+  let browser = bcdBrowser
+
+  if (browser === 'samsunginternet_android') {
+    browser = 'samsung'
+  } else if (browser === 'safari_ios') {
+    browser = 'ios_saf'
+  } else if (browser === 'opera_android') {
+    browser = 'op_mob'
+  } else if (browser === 'chrome_android') {
+    browser = 'and_chr'
+  } else if (browser === 'firefox_android') {
+    browser = 'and_ff'
+  } else if (browser === 'webview_android') {
+    browser = 'android'
+  }
+
+  return browser
+}
+
+let mappedData = {}
+Object.keys(autofillSupportData).forEach(browser => {
+  let browserDataRaw = autofillSupportData[browser]
+  let browserData
+
+  if (Array.isArray(browserDataRaw)) {
+    if (browserDataRaw[0].version_added === 'preview') {
+      browserData = browserDataRaw[1]
+    } else {
+      browserData = browserDataRaw[0]
+    }
+  } else {
+    browserData = browserDataRaw
+  }
+
+  if (browserData.version_added) {
+    mappedData[bcdBrowserToCanIUseBrowser(browser)] = {
+      prefix: browserData.prefix,
+      versionAdded: browserData.version_added
+    }
+  }
+})
+
+console.log(mappedData)
