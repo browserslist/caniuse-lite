8557ed13f3934c03e0d847cefc656628b7d5ad69
Andrey Sitnik <andrey@sitnik.ru>
2021-12-19 14:21:40 +0300

    Move from yarn 1 to pnpm

--- commit.js
+++ commit.js
@@ -2,6 +2,6 @@ const { execSync } = require('child_process')
 
 const pkg = require('./package.json')
 
-execSync('git add data/ package.json yarn.lock')
+execSync('git add data/ package.json pnpm-lock.yaml')
 execSync(`git commit -m "Update caniuse-db ${pkg.version}"`)
 execSync(`git tag -m "Release caniuse-db ${pkg.version}" ${pkg.version}`)
