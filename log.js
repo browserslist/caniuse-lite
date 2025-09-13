70b29760c4dc81ef20b28959507940549ad9071f
Andrey Sitnik <andrey@sitnik.ru>
2025-09-13 21:38:09 +0000

    Print log on CI to save details

--- log.js
+++ log.js
@@ -1,8 +1,10 @@
-const { readFile } = require('node:fs').promises
+const { readFile } = require('node:fs/promises')
 
 readFile('./publish.log').then(log => {
   if (log.toString().includes('npm ERR!')) {
     process.stderr.write(log.toString())
     process.exit(1)
+  } else {
+    process.stdout.write(log.toString())
   }
 })
